8d80521084ebb36e99081f81563aa52341c851b8
Sergio Torres <sergio.torres@idr.edu.mx>
2024-06-25 16:49:46 -0600

    ci: get api rick and morty

--- js/js01-tipos-de-datos/js07-apiFetch.js
+++ js/js01-tipos-de-datos/js07-apiFetch.js
@@ -36,14 +36,60 @@ const getSopita = ( url )=>{
     } );
 }
 
-const getPersonas = ( url ) =>{
+
+const convertirDeDuendeMalita = ( data )=> data.map( user=> ({name: user.firstName }) );
+const convertirDeRickAndMorty = ( data )=> data.results.map( user=> ({name: user.name }) );
+const insertarTarjetas = ( users) =>{
+    const cards = users.map(  user => `
+     <div class="col-lg-4 col-md-6 col-sm-10">
+        <div class="card m-2">
+            <div class="card-body">
+               ${user.name}
+            </div>
+        </div>                
+    </div>
+        ` );
+    return cards.join("");
+}
+
+const buttonSection = document.getElementById("arreglo-botones");
+
+const getPersonas = ( url, convertirAUsuarios ) =>{
 
    // fetch( url ).then( ).catch();
     fetch( url )
         .then( (resolve)=> resolve.json() ) // Convierte de JSON a object
-        .then( (personas)=> console.log(personas)   )
+        .then( (data)=> {
+            // console.log(data);
+            const users = convertirAUsuarios( data );
+            console.log( users );            
+            buttonSection.innerHTML = insertarTarjetas( users );
+        } )           
         .catch( (error)=> console.log(error)  );
 
 };
-getPersonas( "http://3.87.106.38/api/v1/users" );
-getPersonas( "https://rickandmortyapi.com/api/character" );
+getPersonas( "http://3.87.106.38/api/v1/users", convertirDeDuendeMalita );
+//getPersonas( "https://rickandmortyapi.com/api/character", convertirDeRickAndMorty );
+
+
+
+/*
+ Las promesa es un patrón asincrónico que se utiliza
+ para manejer operaciones asíncronas.
+
+ Las promesas permite realizar tareas asíncronas y
+ manejar los resultados (éxito o error) en un momento posterior.
+
+ Las promesas tiene 3 estados:
+
+ 1.- Pending: El estado inicial de una promesa antes de que se resuelva o rechace
+ 2.- Resolved: La promesa se resuelve con un valor
+ 3.- Rejected: La promesa se rechaza con un razón
+
+Resolve y reject con funciones de callback.
+Para crear una promesa se usa el constructor de la clase Promise.
+
+sintaxis:
+    const pinkyPromise = new Promise ( fncCallBack );
+
+*/
