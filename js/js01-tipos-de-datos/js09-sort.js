7e29ce1d244446abca36a6f1992e69b788e61079
Sergio Torres <sergio.torres@idr.edu.mx>
2024-07-11 17:20:41 -0600

    add: sort con metodo de ordenamiento

--- js/js01-tipos-de-datos/js09-sort.js
+++ js/js01-tipos-de-datos/js09-sort.js
@@ -1,16 +1,12 @@
 
 const nombres = ["Diana", "alma","Estefania", "Arantxa", "Yoli"]
 
-nombres.sort();
+nombres.sort( (a,b)=> a.toLowerCase().localeCompare(b.toLowerCase()) );
 
 console.log( nombres ); 
-// [ 'Arantxa', 'Diana', 'Estefania', 'Yoli', 'alma' ]
+// [ 'alma', 'Arantxa', 'Diana', 'Estefania', 'Yoli' ]
 
 const numbers = [12, 322, 34, 2, 9];
-numbers.sort( (a,b)=>{
-    if(a<b) return -1;
-    else if(b>a) return 1;
-    else return 0;
-});
+numbers.sort( (a,b)=> b-a );
 // [ 2, 9, 12, 34, 322 ]
 console.log( numbers );
