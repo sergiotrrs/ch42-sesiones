2784bfe5d00f2f022929dc5d2dc70c0369cb04ac
Sergio Torres <sergio.torres@idr.edu.mx>
2024-07-11 14:31:34 -0600

    solucion de fizzbuzz

--- java/eclipse/java04_ciclos/src/org/generation/CicloFor.java
+++ java/eclipse/java04_ciclos/src/org/generation/CicloFor.java
@@ -94,7 +94,20 @@ public class CicloFor {
 		 *  
 		 */
 		
-		
+		int fin = 20;
+
+		for (int i = 1; i <= fin; i++ ) {
+			
+			if ( i % 3 == 0 && i % 5 == 0 ) {
+				System.out.println("fizzbuz");
+			} else if ( i % 3 == 0 ) {
+				System.out.println("fizz");
+			} else if ( i % 5 == 0 ) {
+				System.out.println("buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
 		
 		
 	}
