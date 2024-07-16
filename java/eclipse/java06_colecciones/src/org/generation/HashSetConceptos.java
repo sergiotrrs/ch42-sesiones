546ff2464146f387f4790c7209efaf52724954e8
Sergio Torres <sergio.torres@idr.edu.mx>
2024-07-16 11:16:18 -0600

    add: colecciń hashset

---
+++ java/eclipse/java06_colecciones/src/org/generation/HashSetConceptos.java
@@ -0,0 +1,78 @@
+package org.generation;
+
+import java.util.HashSet;
+
+public class HashSetConceptos {
+
+	public static void main(String[] args) {
+		/*
+		 * Una colección de tipo set es una colección NO ordenada.
+		 *  - No se permiten elementos duplicados
+		 *  - cualquier elemento duplicado no se insertará
+		 *  
+		 *  Sintaxis:
+		 *    HashSet<Object> varName = new HashSet<>();
+		 */
+		// Definir una colección de nombre de paises con HashSet(String)
+		HashSet <String> paises = new HashSet<>();
+		
+		// Agregamos valores con add()
+		paises.add("Japón");
+		paises.add("Colombia");
+		paises.add("Suecia");
+		paises.add("Canadá");
+		paises.add("Alemania");
+		paises.add("Reino Unido");
+		paises.add("Genovia");
+		paises.add("Narnia");
+		paises.add("Wakanda");
+
+		// Los elementos duplicados no se insertan
+		paises.add("Colombia"); // Maricela
+		paises.add("Japón"); // Arantxa
+		
+		System.out.println( paises );
+		
+		// Verificar si el elemento se insertó
+		System.out.println("Es insertado: " + paises.add("Nueva Zelanda"));
+		System.out.println("Es insertado: " + paises.add("Wakanda"));
+		
+		// Obtenemos el tamaño de la colección con size()
+		System.out.println("Núm. paises: " + paises.size()); // 10
+		
+		// Verificar si existe un elemento con contains()
+		System.out.println("Existe Narnia: " + paises.contains("Narnia"));
+		
+		// Remover un elemento con remove()
+		System.out.println("Eliminado Wakanda? " + paises.remove("Wakanda"));
+		System.out.println("Eliminado USA? " + paises.remove("USA"));
+		
+		// Iterar la colección
+		for (String pais : paises) {
+			System.out.println(pais);
+		}
+		
+		// Crear una colección ArrayList de 15 elementos de String
+		// duplica por lo menos 5 elementos
+		//
+		// Muestra solo los elementos únicos
+		
+		// Paso 1: Crear un Array List de String de Dulces
+		
+		// PAso 2: Agregar nombres de dulces, repetir por lo menos 5
+		
+		// Paso 3: Crear una colección Hash Set
+		
+		// Paso 4: Iterar los elementos del Array List
+		        // Insertar los elementos del Array List en el Hash Set
+		
+		// PAso 5: Imprimir el núm de elementos del HashSet
+		//         Imprimir los elementos del HashSet
+		
+		
+		
+		
+
+	}
+
+}
