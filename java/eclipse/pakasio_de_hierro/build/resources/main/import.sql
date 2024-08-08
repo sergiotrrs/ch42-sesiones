404d8b77cdc2210bb5b14bdd37981c529bd93723
Sergio Torres <sergio.torres@idr.edu.mx>
2024-08-08 02:40:15 -0600

    add: se activa el ligin del usuario

--- java/eclipse/pakasio_de_hierro/build/resources/main/import.sql
+++ java/eclipse/pakasio_de_hierro/build/resources/main/import.sql
@@ -1,6 +1,8 @@
 
-INSERT INTO Roles(id, name, description) VALUES (1,"CUSTOMER", "Cliente de la ecommerce");
-INSERT INTO Roles(id, name, description) VALUES (2,"ADMIN", "Administrador de la ecommerce");
-INSERT INTO Roles(id, name, description) VALUES (3,"WAREHOUSE_MANAGER", "Administrador del almacen");
+INSERT INTO roles(id, name, description) VALUES (1,'CUSTOMER', 'Cliente de la ecommerce');
+INSERT INTO roles(id, name, description) VALUES (2,'ADMIN', 'Administrador de la ecommerce');
+INSERT INTO roles(id, name, description) VALUES (3,'WAREHOUSE_MANAGER', 'Administrador del almacen');
 
-INSERT INTO Users(id, firstname, lastname, email, password, active) VALUES (1,"Duende", "Malito", "duende@cenote.como", "123", 1)
+INSERT INTO users(id, firstname, lastname, email, password, active) VALUES (1,'Duende', 'Malito', 'duende@gmail.com', '$2a$10$Ulquu8HMe6Nzfq6h.iz4hOIndHXDr86qDLaT7GYItibzxnyaEPtPG', 1)
+INSERT INTO user_has_roles(user_id, role_id) VALUES (1, 1);
+INSERT INTO user_has_roles(user_id, role_id) VALUES (1, 2);
---
+++ java/eclipse/pakasio_de_hierro/src/main/resources/static/assets/aboutUs-DLWX3O0z.js
@@ -0,0 +1,1 @@
+import{n as r}from"./navbar-app-BsLDXIwH.js";document.querySelector("#navbar-app").innerHTML=r();
