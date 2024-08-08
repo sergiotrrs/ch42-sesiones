7f9b5288ebef13164e99f68ba80593dff35f3ea7
Sergio Torres <sergio.torres@idr.edu.mx>
2024-08-08 01:38:58 -0600

    SQL: user id 1 con role CUSTOMER y ADMIN

--- java/eclipse/pakasio_de_hierro/src/main/resources/import.sql
+++ java/eclipse/pakasio_de_hierro/src/main/resources/import.sql
@@ -4,4 +4,5 @@ INSERT INTO roles(id, name, description) VALUES (2,'ADMIN', 'Administrador de la
 INSERT INTO roles(id, name, description) VALUES (3,'WAREHOUSE_MANAGER', 'Administrador del almacen');
 
 INSERT INTO users(id, firstname, lastname, email, password, active) VALUES (1,'Duende', 'Malito', 'duende@gmail.com', '$2a$10$Ulquu8HMe6Nzfq6h.iz4hOIndHXDr86qDLaT7GYItibzxnyaEPtPG', 1)
+INSERT INTO user_has_roles(user_id, role_id) VALUES (1, 1);
 INSERT INTO user_has_roles(user_id, role_id) VALUES (1, 2);
